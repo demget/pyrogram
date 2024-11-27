2f724e9df0f3fb80099ab4ff1dfbed65dc00e92c
Demian <optigan@protonmail.com>
2024-11-27 19:06:11 +0100

    methods/advanced/save_file: change rnd_id implementation

--- pyrogram/methods/advanced/save_file.py
+++ pyrogram/methods/advanced/save_file.py
@@ -36,13 +36,17 @@ log = logging.getLogger(__name__)
 
 
 class SaveFile:
+    def rnd_id_urandom(self):
+        """Generates a random long integer (8 bytes), which is optionally signed"""
+        return int.from_bytes(os.urandom(8), signed=True, byteorder="little")
+
     async def save_file(
         self: "pyrogram.Client",
         path: Union[str, BinaryIO],
         file_id: int = None,
         file_part: int = 0,
         progress: Callable = None,
-        progress_args: tuple = ()
+        progress_args: tuple = (),
     ):
         """Upload a file onto Telegram servers, without actually sending the message to anyone.
         Useful whenever an InputFile type is required.
@@ -117,7 +121,9 @@ class SaveFile:
             elif isinstance(path, io.IOBase):
                 fp = path
             else:
-                raise ValueError("Invalid file. Expected a file path as string or a binary (not text) file pointer")
+                raise ValueError(
+                    "Invalid file. Expected a file path as string or a binary (not text) file pointer"
+                )
 
             file_name = getattr(fp, "name", "file.jpg")
 
@@ -131,19 +137,26 @@ class SaveFile:
             file_size_limit_mib = 4000 if self.me.is_premium else 2000
 
             if file_size > file_size_limit_mib * 1024 * 1024:
-                raise ValueError(f"Can't upload files bigger than {file_size_limit_mib} MiB")
+                raise ValueError(
+                    f"Can't upload files bigger than {file_size_limit_mib} MiB"
+                )
 
             file_total_parts = int(math.ceil(file_size / part_size))
             is_big = file_size > 10 * 1024 * 1024
             workers_count = 4 if is_big else 1
             is_missing_part = file_id is not None
-            file_id = file_id or self.rnd_id()
+            file_id = file_id or self.rnd_id_urandom()
             md5_sum = md5() if not is_big and not is_missing_part else None
             session = Session(
-                self, await self.storage.dc_id(), await self.storage.auth_key(),
-                await self.storage.test_mode(), is_media=True
+                self,
+                await self.storage.dc_id(),
+                await self.storage.auth_key(),
+                await self.storage.test_mode(),
+                is_media=True,
             )
-            workers = [self.loop.create_task(worker(session)) for _ in range(workers_count)]
+            workers = [
+                self.loop.create_task(worker(session)) for _ in range(workers_count)
+            ]
             queue = asyncio.Queue(1)
 
             try:
@@ -156,7 +169,9 @@ class SaveFile:
 
                     if not chunk:
                         if not is_big and not is_missing_part:
-                            md5_sum = "".join([hex(i)[2:].zfill(2) for i in md5_sum.digest()])
+                            md5_sum = "".join(
+                                [hex(i)[2:].zfill(2) for i in md5_sum.digest()]
+                            )
                         break
 
                     if is_big:
@@ -164,13 +179,11 @@ class SaveFile:
                             file_id=file_id,
                             file_part=file_part,
                             file_total_parts=file_total_parts,
-                            bytes=chunk
+                            bytes=chunk,
                         )
                     else:
                         rpc = raw.functions.upload.SaveFilePart(
-                            file_id=file_id,
-                            file_part=file_part,
-                            bytes=chunk
+                            file_id=file_id, file_part=file_part, bytes=chunk
                         )
 
                     await queue.put(rpc)
@@ -188,7 +201,7 @@ class SaveFile:
                             progress,
                             min(file_part * part_size, file_size),
                             file_size,
-                            *progress_args
+                            *progress_args,
                         )
 
                         if inspect.iscoroutinefunction(progress):
@@ -205,14 +218,13 @@ class SaveFile:
                         id=file_id,
                         parts=file_total_parts,
                         name=file_name,
-
                     )
                 else:
                     return raw.types.InputFile(
                         id=file_id,
                         parts=file_total_parts,
                         name=file_name,
-                        md5_checksum=md5_sum
+                        md5_checksum=md5_sum,
                     )
             finally:
                 for _ in workers:
